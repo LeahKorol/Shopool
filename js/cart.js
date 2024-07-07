b8cef6ddcb89ec718acbb7b28fc8c05979a9c857
Talya <talya8399@gmail.com>
2024-07-07 08:18:04 +0300

    fix the cart page + empty cart

--- js/cart.js
+++ js/cart.js
@@ -2,43 +2,56 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function rendercart() {
         const cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-        console.log(cart);
-
+        const emptyCartDiv = document.getElementById('empty-cart');
         const productsContainer = document.getElementById('products-container');
-        productsContainer.innerHTML = '';
 
-        let sum = 0;
 
-        cart.forEach(product => {
-            const productElement = document.createElement('div');
-            productElement.classList.add('product-item');
-            productElement.innerHTML = `
-                <div class="product-link">
-                    <div class="choose-item">
-                        <input type="checkbox" id="buy-now-${product.id}" name="buy-now" checked>
+        if (cart.length === 0) {
+            productsContainer.style.display = 'none';
+            emptyCartDiv.style.display = 'flex';
+            document.querySelector('.total-summary').style.display = 'none';
+        }
+
+        else {
+            productsContainer.style.display = 'block';
+            emptyCartDiv.style.display = 'none';
+            document.querySelector('.total-summary').style.display = 'block';
+
+            productsContainer.innerHTML = '';
+
+            let sum = 0;
+
+            cart.forEach(product => {
+                const productElement = document.createElement('div');
+                productElement.classList.add('product-item-in-list');
+                productElement.innerHTML = `
+                    <div class="product-link">
+                        <div class="choose-item">
+                            <input type="checkbox" id="buy-now-${product.id}" name="buy-now" checked>
+                        </div>
+                        <img class="thumbnail" src="${product.thumbnail}" alt="Product Thumbnail">
+                        <div class="product-details"> 
+                            <h3>${product.title}</h3>
+                            <p>Price: $${product.price}</p>
+                            <p>Quantity: ${product.quantity}</p>
+                        </div>
                     </div>
-                    <img class="thumbnail" src="${product.thumbnail}" alt="Product Thumbnail">
-                    <div class="product-details"> 
-                        <h3>${product.title}</h3>
-                        <p>Price: $${product.price}</p>
-                        <p>Quantity: ${product.quantity}</p>
-                    </div>
-                    <button class="delete-product-btn" onclick="deleteProduct(${product.id})">Delete</button>
-                </div>
-            `;
+                    <i class="fas fa-trash delete-product-icon" title="Delete" onclick="deleteProduct(${product.id})"></i>
+                `;
 
-            productsContainer.appendChild(productElement);
+                productsContainer.appendChild(productElement);
 
-            productElement.querySelector('.product-link').addEventListener('click', function () {
-                localStorage.setItem('selectedProduct', JSON.stringify(product));
-                window.location.href = './product-details.html';
+                productElement.querySelector('.product-link').addEventListener('click', function () {
+                    localStorage.setItem('selectedProduct', JSON.stringify(product));
+                    window.location.href = './product-details.html';
+                });
+
+                sum += product.price * product.quantity;
             });
 
-            sum += product.price;
-        });
-
-        document.getElementById('total').textContent = sum.toFixed(2);
+            document.getElementById('total').textContent = '$' + sum.toFixed(2);
+        }
+        
     }
 
     window.deleteProduct = function (productId) {
